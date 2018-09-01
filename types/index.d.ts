c8c151edeef6274c8476e2145fb0a5be1b3aae17
MrJohnCoder <johnsmithcoder7@gmail.com>
2018-09-01 19:01:52 -0400

    fill rest of types besides module options

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,17 +3,26 @@ declare module 'eris-sharder' {
 
     export class Sharder {
         constructor(modules: Modules, options: SharderOptions);
+        public modules: Modules;
+        public options: SharderOptions;
     }
 
     export class Logger {
-        constructor();
+        constructor(options: LoggerOptions);
+        public options: LoggerOptions;
         public transports: Transport[];
         public init(): Promise<[]>;
         public registerTransport(transport: Transport);
-        public debug(data: );
+        public debug(data: Json);
+        public error(data: Json);
+        public info(data: Json);
+        public log(data: Json);
+        public warn(data: Json);
     } 
 
     export class Communication extends EventEmitter {
+        constructor(logger: Logger);
+        public logger: Logger;
         public init(): Promise<void>;
         public send(instance: string, clusterID: string, event: string, data: Json);
         public awaitResponse(instance: string, clusterID: string, event: string, data: Json): Promise<Json>;
@@ -22,19 +31,40 @@ declare module 'eris-sharder' {
     }
 
     export class Clustering {
-
+        constructor(options: ClusteringOptions, communication: Communication, sharding: Sharding, logger: Logger);
+        public options: ClusteringOptions;
+        public communication: Communication;
+        public sharding: Sharding;
+        public logger: Logger;
+        get isMaster(): boolean;
+        public init();
     }
 
     export class Sharding {
-
+        constructor(options: ShardingOptions, token: string, logger: Logger);
+        public options: ShardingOptions;
+        public token: string;
+        public logger: Logger;
+        public init();
     }
 
     export class Stats {
-
+        constructor(options: StatsOptions, communication: Communication, logger: Logger);
+        public options: StatsOptions;
+        public communication: Communication;
+        public logger: Logger;
+        public init();
     }
 
     export class Transport {
-
+        constructor(options: TransportOptions);
+        public options: TransportOptions;
+        public init(): Promise<void>;
+        public debug(data: Json);
+        public error(data: Json);
+        public info(data: Json);
+        public log(data: Json);
+        public warn(data: Json);
     }
 
     type Modules = {
@@ -49,7 +79,8 @@ declare module 'eris-sharder' {
         token: string,
         sharding: ShardingOptions,
         clustering: ClusteringOptions,
-        stats: StatsOptions
+        stats?: StatsOptions,
+        logger?: LoggerOptions
     }
 
     type ShardingOptions = {
@@ -61,6 +92,14 @@ declare module 'eris-sharder' {
     }
 
     type StatsOptions = {
+
+    }
+
+    type LoggerOptions = {
+
+    }
+
+    type TransportOptions = {
 
     }
 
