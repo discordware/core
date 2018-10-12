10f6e696af9c4842c190c02d001ce0004ae2ddaf
JohnCoder <johnsmithcoder7@gmail.com>
2018-10-11 20:19:16 -0400

    fill out cluster class

--- src/Cluster.js
+++ src/Cluster.js
@@ -1,10 +1,39 @@
+const Communication = require('./cluster/Communication');
+
 class Cluster {
-    constructor() {
-        
-    } 
+    constructor(options, modules) {
+        this.options = options;
+        this.modules = modules;
+    }
+
+    get token() {
+        return process.env.TOKEN;
+    }
+
+    get firstShardID() {
+        return process.env.FIRST_SHARD_ID;
+    }
+
+    get lastShardID() {
+        return process.env.LAST_SHARD_ID;
+    }
+
+    get maxShards() {
+        return process.env.MAX_SHARDS;
+    }
+
+    get instanceID() {
+        return process.env.INSTANCE_ID;
+    }
+
+    get clusterID() {
+        return process.env.CLUSTER_ID;
+    }
 
     async init() {
-        await this.base.init();
+        this.communication = this.modules.communication || new Communication(this.options.communication);
+
+        await this.communication.init();
 
         return Promise.resolve();
     }
